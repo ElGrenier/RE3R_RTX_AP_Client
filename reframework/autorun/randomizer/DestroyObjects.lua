5741c8a5dbde575bcaa95142012ffebae3275107
Solidus Snake <63137482+dontjoome@users.noreply.github.com>
2025-02-22 15:39:38 -0500

    Initial Upload
    Feature parity with 0.2.3 non-RTX, aside from RTX support :smile:

---
+++ reframework/autorun/randomizer/DestroyObjects.lua
@@ -0,0 +1,43 @@
+local DestroyObjects = {}
+DestroyObjects.isInit = false
+DestroyObjects.lastRemoval = os.time()
+
+function DestroyObjects.Init()
+    if Archipelago.IsConnected() and not DestroyObjects.isInit then
+        DestroyObjects.isInit = true
+        DestroyObjects.DestroyAll()
+    end
+
+    -- Reset the init flag every 15 seconds
+    if os.time() - DestroyObjects.lastRemoval > 15 then -- 15 seconds
+        DestroyObjects.isInit = false
+    end
+end
+
+function DestroyObjects.DestroyAll()
+    local destroyables = {
+        DestroyObjects.GetPurposeGUI()
+    }
+
+    -- Only add the door to destroyables if talkedToTyrell is true
+    if Storage.talkedToTyrell then
+        table.insert(destroyables, DestroyObjects.GetMainHallDoor())
+    end
+
+    -- Destroy all objects in the table
+    for k, obj in pairs(destroyables) do
+        if obj ~= nil then
+            obj:call("destroy", obj)
+        end        
+    end
+end
+
+function DestroyObjects.GetPurposeGUI()
+    return scene:call("findGameObject(System.String)", "GUI_Purpose")
+end
+
+function DestroyObjects.GetMainHallDoor()
+    return scene:call("findGameObject(System.String)", "Door_2_1_030w_gimmick")
+end
+
+return DestroyObjects
---
+++ reframework/autorun/randomizer/Manifest.lua
@@ -0,0 +1,6 @@
+local Manifest = {}
+
+Manifest.mod_name = "ArchipelagoRE3R"
+Manifest.version = "0.2.3"
+
+return Manifest
